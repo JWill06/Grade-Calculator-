8d0a5b2e64bf5f85a77e61fb72c49d5a2a3796e8
JWill06 <127267694+JWill06@users.noreply.github.com>
2023-12-03 17:07:51 -0500

    Update OverallGrade.cs

--- OverallGrade.cs
+++ OverallGrade.cs
@@ -11,20 +11,65 @@ using System.Windows.Forms;
 
 namespace GradeCalculator
 {
-    
-    public partial class OverallGrade : UserControl
-    {
-        DB database;
+    // Replace with your actual connection string
+        string connectionString = "Server=; " +
+            "Trusted_Connection=true;" +
+            "Database=StudentDatabase;" +
+            "User Instance=false;" +
+            "Connection Timeout=30";
+
+        // Remove the hard-coded value
+        public int StudentID { get; set; } = 2;
+
+        // Set to a known good CourseID for testing
+        public int CourseID = 101;
+
         public OverallGrade()
         {
             InitializeComponent();
-            database = new DB();
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            double percentage = database.CalculateOverallPercentage("YourStoredProcedureName");
-            lb_percentageOverall.Text = percentage.ToString();
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    using (SqlCommand command = new SqlCommand("GetOverallStudentGrade", connection))
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+                        command.Parameters.AddWithValue("@StudentID", StudentID);
+                        command.Parameters.AddWithValue("@CourseID", CourseID);
+
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                string overallLetterGrade = reader["OverallLetterGrade"].ToString();
+                                string overallPercentage = reader["OverallGradePercentage"].ToString();
+
+                                lb_letterGradeOverall.Text = overallLetterGrade;
+                                lb_percentageOverall.Text = overallPercentage + "%";
+                            }
+                            else
+                            {
+                                lb_letterGradeOverall.Text = "No data found";
+                                lb_percentageOverall.Text = "No data found";
+                            }
+                        }
+                    }
+                }
+            }
+            catch (SqlException sqlEx)
+            {
+                MessageBox.Show("SQL Error: " + sqlEx.Message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("General Error: " + ex.Message);
+            }
         }
     }
 }
