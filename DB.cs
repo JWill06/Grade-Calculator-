6ad2977a65e9f1d2e38cfd167534060c19ea60ef
JWill06 <127267694+JWill06@users.noreply.github.com>
2023-12-12 15:15:27 -0500

    Update DB.cs

--- DB.cs
+++ DB.cs
@@ -23,84 +23,50 @@ namespace GradeCalculator
                                "Connection Timeout=30";
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        public DB(string conn)
         {
-            string assignment = txtAssignment.Text;
-            int pointsEarned, pointsPossible;
+            connectionString = conn;
+        }
 
-            if (!int.TryParse(txtPointsEarned.Text, out pointsEarned) || !int.TryParse(txtPointsPossible.Text, out pointsPossible))
-            {
-                MessageBox.Show("Invalid input for points earned or points possible. Please enter valid numbers.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                return;
-            }
-
+        public string GetStudentName(int studentID)
+        {
+            string query = "SELECT FirstName, LastName FROM Student WHERE StudentID = @StudentID";
             try
             {
-                //Add the Course ID (150) as a parameter
-                int courseId = 101;
-                int studentId = 1;
-                int assignmentId = int.Parse(txtAssignment.Text);
-
                 using (SqlConnection connection = new SqlConnection(connectionString))
                 {
-                    connection.Open();
-
-                    //Check if PK value already exists
-                    using (SqlCommand checkCommand = new SqlCommand("SELECT COUNT(*) FROM dbo.Assignment WHERE AssignmentID = @AssignmentID AND CourseID = @CourseID", connection))
+                    using (SqlCommand command = new SqlCommand(query, connection))
                     {
-                        checkCommand.Parameters.AddWithValue("@AssignmentID", assignmentId);
-                        checkCommand.Parameters.AddWithValue("@StudentID", studentId);
-                        checkCommand.Parameters.AddWithValue("@CourseID", courseId);
+                        command.Parameters.AddWithValue("@StudentID", studentID);
+                        connection.Open();
 
-                        int count = (int)checkCommand.ExecuteScalar();
-
-                        if (count > 0)
+                        using (SqlDataReader reader = command.ExecuteReader())
                         {
-                            //a record with the sam PK already exists
-                            MessageBox.Show("A record for this assignment already exists");
-                        }
-                        else
-                        {
-                            //No record of same PK 
-
-                            using (SqlCommand command = new SqlCommand("INSERT INTO Assignment (AssignmentID, AssignmentName,  StudentID, PointsEarned, MaxPoints, CourseID) VALUES (@AssignmentID, @AssignmentName, @StudentID, @PointsEarned, @MaxPoints, @CourseID)", connection))
+                            if (reader.Read())
                             {
-                                command.Parameters.AddWithValue("@AssignmentID", assignmentId);
-                                command.Parameters.AddWithValue("@AssignmentName", assignment);
-                                command.Parameters.AddWithValue("@StudentID", studentId);
-                                command.Parameters.AddWithValue("@PointsEarned", pointsEarned);
-                                command.Parameters.AddWithValue("@MaxPoints", pointsPossible);
-                                command.Parameters.AddWithValue("@CourseID", courseId);
-
-                                //command.ExecuteNonQuery();
-                                int rowsAffected = command.ExecuteNonQuery();
-
-                                if (rowsAffected > 0)
-                                {
-                                    MessageBox.Show("Grade submitted successfully.");
-                                }
-                                else
-                                {
-                                    MessageBox.Show("Error submitting grade");
-                                }
+                                string firstName = reader.GetString(0);
+                                string lastName = reader.GetString(1);
+                                return $"{firstName} {lastName}";
                             }
-
-                            //MessageBox.Show("Grade submitted successfully.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
-
+                            else
+                            {
+                                MessageBox.Show("Invalid student ID or student does not exist.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                                return null;
+                            }
                         }
                     }
-                    //catch (Exception ex)
-                    //{
-                    //MessageBox.Show("Error submitting grade: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                    //}
                 }
-
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show($"SQL Error: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return null;
             }
             catch (Exception ex)
             {
-                MessageBox.Show("Error submitting grade: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                MessageBox.Show($"General Error: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return null;
             }
-
         }
     }
 }
